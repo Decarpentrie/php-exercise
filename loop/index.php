2dcaab0a6378bc24ef30a07061c0ba1e69a846c2
Decarpentrie <elodie.decarpentrie32@gmail.com>
2017-06-20 13:54:12 +0200

    exo 4

--- loop/index.php
+++ loop/index.php
@@ -8,22 +8,36 @@
 <?php  
 // exercice 1
 	
-	$i = 0;
-	for ($i = 0; $i <= 10; $i++) {
-  		echo $i;
-	}	
+	// for ($i = 0; $i <= 10; $i++) {
+	// 	if($i<10){
+	// 		echo $i;	
+	// 	}   		
+	// };	
 
 // exercice 2
 
-	for ($k = 0, $j = 50; $k <= 20; $k++){
-		  print $k*$j;
-	};
+	// for ($k = 0, $j = 50; $k <= 20; $k++){
+	// 	if($k < 20){
+	// 	 echo $k*$j;
+	// 	}	 
+	// };
 
 // exercice 3
 
-	for ($e=100, $c = 20; $e >=10 ; $e--) { 
-		print $e*$c;
-	}
+	// for ($e=100, $c = 20; $e >=10 ; $e--) { 
+	// 	if ($e < 100) {
+	// 	echo $e*$c;
+	// 	}
+	// };
+
+// exercice 4
+
+for ($i = 1; $i <= 10; $i++) {
+		if($i<10){
+			echo  $i+$i/2;	
+		}   		
+	};	
+	
 
 ?>
 
