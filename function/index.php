d92a78a9586f0e81b8ca271fa71c6ddb93b2aa8a
Decarpentrie <elodie.decarpentrie32@gmail.com>
2017-06-20 16:29:15 +0200

    exercice 2

--- function/index.php
+++ function/index.php
@@ -1,9 +1,27 @@
 <?php 
 // exercice 1
 	
-	function foo() {
-  echo "true";
-}
-foo();
+	// function foo() {
+	// 	echo "true";
+	// }
+	// foo();
+
+// exercice 2
+
+	function string($string="coucou"){
+		echo $string;
+	}
+	string();
+
+
+
+
+
+
+
+
+
+
+
 
 ?>
