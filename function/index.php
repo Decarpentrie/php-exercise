c3cde1b39417353ded06ce2339290d7939c9a8c2
Decarpentrie <elodie.decarpentrie32@gmail.com>
2017-06-20 16:57:04 +0200

    exercice 4

--- function/index.php
+++ function/index.php
@@ -3,26 +3,35 @@
 	
 	// function foo() {
 	// 	echo "true";
-	// }
+	// };
 	// foo();
 
 // exercice 2
 
 	// function word($word="coucou"){
 	// 	echo $word;
-	// }
+	// };
 	// word();
 
 // exercice 3
 
-	function wordTwo($wordOne="youhou", $wordWord="hop"){
-		echo $wordOne, $wordWord;
-	}
-	wordTwo();
+	// function wordTwo($wordOne="youhou", $wordWord="hop"){
+	// 	echo $wordOne, $wordWord;
+	// };
+	// wordTwo();
 
+// exercice 4
 
-
-
+	function number($a=15, $b=13){
+		if ($a>$b){
+			echo "le premier nombre est plus grand";
+		}else if($a<$b){
+			echo "le premier nombre est plus petit";
+		}else{
+			echo "les deux nombres sont identique";
+		};
+	};
+	number()
 
 
 
