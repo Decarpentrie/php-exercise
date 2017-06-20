1f257b1e5f2a1291f71406a586363dd3ca9415af
Decarpentrie <elodie.decarpentrie32@gmail.com>
2017-06-20 12:16:00 +0200

    1er exercie fait

---
+++ loop/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<?php  
+// exercice 1
+	
+	$i = 0;
+	for ($i = 0; $i <= 10; $i++) {
+  		echo $i;
+	}	
+
+?>
+
+</body>
+</html>
