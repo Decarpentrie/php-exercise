1ffc6faf08ebd8d1ac8734d017e448fbd05977f1
Decarpentrie <elodie.decarpentrie32@gmail.com>
2017-06-21 10:40:14 +0200

    exercice 1

---
+++ array/index.php
@@ -0,0 +1,16 @@
+<?php 
+
+// exercie 1
+
+	$mois= array("Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Decembre");
+
+
+
+
+
+
+
+
+
+
+ ?>
