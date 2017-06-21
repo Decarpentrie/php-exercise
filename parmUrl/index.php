b805e874e07a98f6b46857dafe9cb22cdd2cd25c
Decarpentrie <elodie.decarpentrie32@gmail.com>
2017-06-21 17:20:02 +0200

    exercice 4

--- parmUrl/index.php
+++ parmUrl/index.php
@@ -5,4 +5,7 @@
 <!-- <a href="index2.php?nom=Dupont&amp;prenom=Jean&amp;age=12ans">Dis-moi bonjour !</a> -->
 
 <!-- exercice 3 -->
-<a href="index2.php?dateDebut=2/05/2016&amp;dateFin=27/11/2016">Date</a>
+<!-- <a href="index2.php?dateDebut=2/05/2016&amp;dateFin=27/11/2016">Date</a> -->
+
+<!-- exercice 4 -->
+<a href="index2.php?langage=PHP&amp;serveur=LAMP">lanserv</a>
--- parmUrl/index2.php
+++ parmUrl/index2.php
@@ -5,4 +5,7 @@
 <!-- <p>Bonjour <?php echo $_GET['prenom']. ' ' . $_GET['nom']. ' '. $_GET['age']; ?> !</p> -->
 
 <!-- exercice 3 -->
-<p>date: <?php echo $_GET['dateDebut']. ' ' . $_GET['dateFin']; ?> </p>
+<!-- <p>date: <?php echo $_GET['dateDebut']. ' ' . $_GET['dateFin']; ?> </p> -->
+
+<!-- exercice 4 -->
+<p>langage et serveur: <?php echo $_GET['langage']. ' ' . $_GET['serveur']; ?> </p>
