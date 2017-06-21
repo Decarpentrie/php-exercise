79f4c89bf1c6b0541fda79dfe3471ea8a007491c
Decarpentrie <elodie.decarpentrie32@gmail.com>
2017-06-21 11:32:45 +0200

    exercice 2

--- array/index.php
+++ array/index.php
@@ -2,9 +2,24 @@
 
 // exercie 1
 
-	$mois= array("Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Decembre");
+	$mois= array(
+		"Janvier", 
+		"Février", 
+		"Mars", 
+		"Avril", 
+		"Mai", 
+		"Juin", 
+		"Juillet", 
+		"Aout", 
+		"Septembre", 
+		"Octobre", 
+		"Novembre", 
+		"Decembre"
+	);
 
+// exercie 2
 
+print_r($mois[5])
 
 
 
