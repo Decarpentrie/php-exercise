07d8be99a3ffd0a78f06f5a3a90fa703a2fd1c20
Decarpentrie <elodie.decarpentrie32@gmail.com>
2017-06-21 11:54:07 +0200

    exercice 5

--- array/index.php
+++ array/index.php
@@ -3,19 +3,19 @@
 // exercie 1
 
 	$mois= array(
-		"Janvier", 
-		"Février", 
-		"Mars", 
-		"Avril", 
-		"Mai", 
-		"Juin", 
-		"Juillet", 
-		"Aout",
-		"Septembre", 
-		"Octobre", 
-		"Novembre", 
-		"Decembre"
-	);
+			"Janvier", 
+			"Février", 
+			"Mars", 
+			"Avril", 
+			"Mai", 
+			"Juin", 
+			"Juillet", 
+			"Aout",
+			"Septembre", 
+			"Octobre", 
+			"Novembre", 
+			"Decembre"
+		);
 
 // exercice 2
 
@@ -27,12 +27,20 @@
 
 // exercice 4
 
-	$mois[7]= "Août";
-	var_dump($mois)
+	// $mois[7]= "Août";
+	// var_dump($mois);
 
+// exercice 5
 
+	$region= array(
+			02	=> "aisne",
+			59	=> "nord",
+			60  => "oise",
+			62	=> "Pas-de-calais",
+			80	=> "somme",
+		);
 
-
-
+	var_dump($region);
+ 
 
  ?>
