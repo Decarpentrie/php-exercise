9394d0f0609c626ea2e178393cb29a459478f590
Decarpentrie <elodie.decarpentrie32@gmail.com>
2017-06-22 15:17:28 +0200

    exercice 3 et 4

--- date/index.php
+++ date/index.php
@@ -12,4 +12,8 @@
 	// exercice 3
 	setlocale(LC_TIME, 'fr_FR.UTF8');
 	echo '<br>'. strftime('%A %d %B %Y ');
+
+	// exercice 4
+	setlocale(LC_TIME, 'fr_FR.UTF8');
+	echo '<br>'. strftime('%A %d %B %Y, %H:%M');	
  ?>
