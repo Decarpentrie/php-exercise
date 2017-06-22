5b60f3b482db17f0b8e1c991e77d3114400d882d
Decarpentrie <elodie.decarpentrie32@gmail.com>
2017-06-22 15:33:21 +0200

    exercice 5

--- date/index.php
+++ date/index.php
@@ -14,6 +14,35 @@
 	echo '<br>'. strftime('%A %d %B %Y ');
 
 	// exercice 4
-	setlocale(LC_TIME, 'fr_FR.UTF8');
-	echo '<br>'. strftime('%A %d %B %Y, %H:%M');	
+	// echo '<br>' time();
+
+	// exercice 5
+	$now   = time();
+	$date2 = strtotime('2-08-2017');
+	
+	function dateDiff($date1, $date2){
+    $diff = abs($date1 - $date2);
+    $retour = array();
+ 
+    $tmp = $diff;
+    $retour['second'] = $tmp % 60;
+ 
+    $tmp = floor( ($tmp - $retour['second']) /60 );
+    $retour['minute'] = $tmp % 60;
+ 
+    $tmp = floor( ($tmp - $retour['minute'])/60 );
+    $retour['hour'] = $tmp % 24;
+ 
+    $tmp = floor( ($tmp - $retour['hour'])  /24 );
+    $retour['day'] = $tmp;
+ 
+    return $retour;
+	}
+
+	print_r( dateDiff($now, $date2));
+	
+
+
+
+
  ?>
