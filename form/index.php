2f305b7d45ead57b29e7ebade4306bd0d91eb6cc
Decarpentrie <elodie.decarpentrie32@gmail.com>
2017-06-22 11:28:12 +0200

    exercice 2,3,(4)

--- form/index.php
+++ form/index.php
@@ -4,14 +4,26 @@
 	<title></title>
 </head>
 <body>
-<!-- exercice 1 -->
-	<form action="user.php" method="GET">
+<!-- exercice 1 et 3 -->
+	<!-- <form action="user.php" method="GET">
 		<label>prénom</label>
 		<input type="text" name="prenom">
 		<label>nom</label>
 		<input type="text" name="nom">
 		<input type="submit" value="OK" >
+	</form> -->
+
+<!-- exercice 2 et 4 -->
+	<form action="user.php" method="post">
+		<label>prénom</label>
+		<input type="text" name="nom"/>
+		<label>nom</label>
+		<input type="text" name="prenom"/>
+		<input type="submit" value="OK" />
 	</form>
+
+
+
 
 </body>
 </html>
--- form/user.php
+++ form/user.php
@@ -1,3 +1,7 @@
-<!-- exercice 1 -->
-nom : <?php echo htmlspecialchars($_GET["nom"]); ?>
-<p>prénom : <?php echo htmlspecialchars($_GET["prenom"]); ?></p>
+<!-- exercice 1 et 3 -->
+<!-- nom : <?php echo htmlspecialchars($_GET["nom"]); ?> -->
+<!--<p>prénom : <?php echo htmlspecialchars($_GET["prenom"]); ?></p> -->
+
+<!-- exercice 2 et 4 -->
+nom : <?php echo htmlspecialchars($_POST['nom']);?>
+<p>prénom : <?php echo htmlspecialchars($_POST['prenom']);?></p>
