cfff3cd0d9977beabdd720267c9814dd913544d0
Decarpentrie <elodie.decarpentrie32@gmail.com>
2017-06-22 11:48:58 +0200

    exercice 5

--- form/index.php
+++ form/index.php
@@ -14,22 +14,43 @@
 	</form> -->
 
 <!-- exercice 2 et 4 -->
-	<form action="index.php" method="post">
+<!-- 	<form action="index.php" method="post">
 		<label>prénom</label>
-		<input type="text" name="nom"/>
-		<label>nom</label>
 		<input type="text" name="prenom"/>
+		<label>nom</label>
+		<input type="text" name="nom"/>
 		<input type="submit" value="OK" />
 	</form>
 
-<?php 
-$nom= $_POST['nom'];
-$prenom = $_POST['prenom'];
+	<?php 
+		$nom= $_POST['nom'];
+		$prenom = $_POST['prenom'];
+		echo ("nom: $nom, prenom: $prenom");
+ 	?> -->
 
-echo ("nom: $nom, prenom: $prenom");
+<!-- exercice 5 -->
 
- ?>
+	<form action="index.php" method="post">
+		<label>civilité</label>
+		<select name="civilite">
+			<option>choisir</option>
+			<option value="Mme">Mme</option>
+			<option value="Mr">Mr</option>
+		</select>
+		<label>nom</label>
+		<input type="text" name="nom"/>
+		<label>prénom</label>
+		<input type="text" name="prenom"/>
+		<input type="submit" value="OK" />
+	</form>
+	
+	<?php 
+	$choix=$_POST['civilite'];
+	$nom=$_POST['nom'];
+	$prenom=$_POST['prenom'];
+		echo "coucou $choix $nom $prenom !" ;
 
+	?>
 
 </body>
 </html>
